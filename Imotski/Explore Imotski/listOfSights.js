ddb5e8c30e8e4efc07189ae56b2a6f709a144bd1
jopaa10 <jopamate@gmail.com>
2021-07-24 14:14:51 +0200

    replacing flatlist with view

--- Imotski/Explore Imotski/listOfSights.js
+++ Imotski/Explore Imotski/listOfSights.js
@@ -5,70 +5,83 @@ import {
   StyleSheet,
   Text,
   StatusBar,
-  FlatList,
-  SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faMapMarkerAlt, faHeart} from '@fortawesome/free-solid-svg-icons';
 
-const DATA = [
-  {
-    id: '1',
-    image: require('../images/blueLakeH.jpg'),
-    title: 'Imotski',
-  },
-  {
-    id: '2',
-    image: require('../images/biokovo.jpg'),
-    title: 'Biokovo',
-    marginLeft: 5,
-    backgroundColor: 'blue',
-  },
-  {
-    id: '3',
-    image: require('../images/dvaOkaH.jpg'),
-    title: 'Prolozac',
-    marginLeft: 10,
-  },
-  {
-    id: '4',
-    image: require('../images/greenLakeH.jpg'),
-    title: 'Ricice',
-  },
-];
-
-const Item = ({item}) => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={item.image} />
-      <FontAwesomeIcon icon={faHeart} style={styles.iconHeart} size={20} />
-      <View style={styles.cityContainer}>
-        <FontAwesomeIcon
-          icon={faMapMarkerAlt}
-          style={styles.iconLocation}
-          size={12}
-        />
-        <Text style={[styles.cityTitle, {marginLeft: item.marginLeft}]}>
-          {item.title}
-        </Text>
-      </View>
-    </View>
-  );
-};
-
 const ListOfSights = () => {
-  const renderItem = ({item}) => {
-    return <Item item={item} />;
-  };
-
   return (
-    <SafeAreaView>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require('../images/blueLakeH.jpg')}
+          />
+          <FontAwesomeIcon icon={faHeart} style={styles.iconHeart} size={20} />
+          <View style={styles.cityContainer}>
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              style={styles.iconLocation}
+              size={12}
+            />
+            <Text style={[styles.cityTitle, {marginLeft: 0}]}>Imotski</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require('../images/biokovo.jpg')}
+          />
+          <FontAwesomeIcon icon={faHeart} style={styles.iconHeart} size={20} />
+          <View style={styles.cityContainer}>
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              style={styles.iconLocation}
+              size={12}
+            />
+            <Text style={[styles.cityTitle, {marginLeft: 0}]}>Biokovo</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require('../images/dvaOkaH.jpg')}
+          />
+          <FontAwesomeIcon icon={faHeart} style={styles.iconHeart} size={20} />
+          <View style={styles.cityContainer}>
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              style={styles.iconLocation}
+              size={12}
+            />
+            <Text style={[styles.cityTitle, {marginLeft: 5}]}>Prolozac</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require('../images/greenLakeH.jpg')}
+          />
+          <FontAwesomeIcon icon={faHeart} style={styles.iconHeart} size={20} />
+          <View style={styles.cityContainer}>
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              style={styles.iconLocation}
+              size={12}
+            />
+            <Text style={[styles.cityTitle, {marginLeft: 0}]}>Ricice</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </>
   );
 };
 
