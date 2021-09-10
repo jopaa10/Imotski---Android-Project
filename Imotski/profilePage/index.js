796dc3e6645277df3388af4bff88d37d02e3ced7
jopaa10 <jopamate@gmail.com>
2021-09-10 22:12:26 +0200

    adding styles to user profile page

--- Imotski/profilePage/index.js
+++ Imotski/profilePage/index.js
@@ -52,7 +52,7 @@ export const ProfilePage = () => {
 
   return (
     <>
-      <ScrollView style={{backgroundColor: 'white'}}>
+      <ScrollView style={{backgroundColor: 'grey'}}>
         <View style={styles.containerWhite}>
           <Text style={styles.coverText}>ADD COVER PHOTO</Text>
         </View>
@@ -68,10 +68,14 @@ export const ProfilePage = () => {
               d="M0 0L17.2987 9.05882C33.2667 18.1176 66.5333 34.7255 99.8 54.3529C133.067 73.9804 166.333 96.6274 199.6 99.6471C232.867 102.667 266.133 86.0588 299.4 63.4118C332.667 40.7647 365.933 12.0784 399.2 19.6275C432.467 28.6863 465.733 73.9804 483.032 96.6274L499 119.275V154H483.032C465.733 154 432.467 154 399.2 154C365.933 154 332.667 154 299.4 154C266.133 154 232.867 154 199.6 154C166.333 154 133.067 154 99.8 154C66.5333 154 33.2667 154 17.2987 154H0V0Z"
               fill="#1F83BB"
             />
+            <View style={styles.userProfilePic}>
+              <FontAwesomeIcon icon={faUser} color={'#BABABA'} size={30} />
+            </View>
           </Svg>
         </View>
         <View style={styles.containerBlue}>
-          <View style={styles.viewUserInfo}>
+          <View
+            style={[styles.viewUserInfo, {marginBottom: windowWidth * 0.05}]}>
             <FontAwesomeIcon
               icon={faUser}
               size={22}
@@ -81,7 +85,6 @@ export const ProfilePage = () => {
             <Text style={styles.textUserInfo}>Name</Text>
             <Text style={styles.textName1}> Peter Pan </Text>
           </View>
-
           <View style={styles.viewUserInfo}>
             <FontAwesomeIcon
               icon={faMapMarkedAlt}
@@ -91,9 +94,9 @@ export const ProfilePage = () => {
             />
             <Text style={styles.textUserInfo}>Place of resistance</Text>
             <Text style={styles.textName1}> Imotski </Text>
-            <View style={styles.editBtn}>
-              <FontAwesomeIcon icon={faPen} color={'white'} size={20} />
-            </View>
+            <Pressable style={styles.editBtn}>
+              <FontAwesomeIcon icon={faPen} color={'white'} size={16} />
+            </Pressable>
           </View>
           <View style={styles.viewUserInfo}>
             <FontAwesomeIcon
@@ -105,10 +108,11 @@ export const ProfilePage = () => {
             <Text style={styles.textUserInfo}>Days of staying</Text>
             <Text style={styles.textName1}> 5 </Text>
             <View style={styles.editBtn}>
-              <FontAwesomeIcon icon={faPen} color={'white'} size={20} />
+              <FontAwesomeIcon icon={faPen} color={'white'} size={16} />
             </View>
           </View>
-          <View style={styles.viewUserInfo}>
+          <View
+            style={[styles.viewUserInfo, {marginBottom: windowWidth * 0.05}]}>
             <FontAwesomeIcon
               icon={faEnvelope}
               size={22}
@@ -128,7 +132,7 @@ export const ProfilePage = () => {
             <Text style={styles.textUserInfo}>New password</Text>
             <Text style={styles.textName1}> ****** </Text>
             <View style={styles.editBtn}>
-              <FontAwesomeIcon icon={faPen} color={'white'} size={20} />
+              <FontAwesomeIcon icon={faPen} color={'white'} size={16} />
             </View>
           </View>
         </View>
@@ -144,24 +148,38 @@ export const ProfilePage = () => {
 
 const styles = StyleSheet.create({
   containerWhite: {
+    position: 'absolute',
+    width: windowWidth,
     backgroundColor: 'white',
-    height: windowHeight * 0.35,
+    height: windowHeight * 0.5,
     justifyContent: 'center',
   },
   coverText: {
     textAlign: 'center',
     color: '#828282',
     fontSize: 16,
+    paddingBottom: windowWidth * 0.4,
   },
   waves: {
     width: windowWidth,
     height: 154,
+    marginTop: windowWidth * 0.35,
+  },
+  userProfilePic: {
+    width: windowWidth * 0.25,
+    height: windowHeight * 0.15,
+    borderRadius: Math.round(windowWidth + windowHeight) / 2,
+    borderColor: '#DADADA',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    marginLeft: windowWidth * 0.08,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   containerBlue: {
     backgroundColor: '#1F83BB',
     marginTop: 0,
     height: 'auto',
-    paddingTop: windowWidth * 0.1,
   },
   viewUserInfo: {
     width: windowWidth * 0.7,
@@ -182,7 +200,7 @@ const styles = StyleSheet.create({
   editBtn: {
     width: windowWidth * 0.85,
     alignItems: 'flex-end',
-    bottom: windowWidth * 0.08,
+    bottom: windowWidth * 0.09,
   },
   btnLogout: {
     borderColor: 'white',
@@ -190,8 +208,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     justifyContent: 'center',
     width: windowWidth * 0.4,
-    height: 60,
-    marginBottom: windowWidth * 0.1,
+    height: 57,
+    marginBottom: windowWidth * 0.2,
   },
   textBtnLogout: {
     textAlign: 'center',
